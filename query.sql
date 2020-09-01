e50ae876730fa8b7bc1394a275cec3d05809eb5c
MonicaKang <mokaex@gmail.com>
2020-08-31 21:14:32 -0700

    update

--- query.sql
+++ query.sql
@@ -2,13 +2,6 @@ Select * from wine;
 
 SELECT wine.id, wine.country, wine.designation, wine.points, wine.price, wine.province, wine.region01 FROM wine;
 
-SELECT points 
-FROM  wine
-
-SELECT id, country, province, variety, points
-FROM wine
-ORDER BY country
-
 SELECT country,
 COUNT(*),
 AVG(points) AS Average_points
