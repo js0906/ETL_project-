2f5a242b4c357756cf884da545bd3ea76c11eb4a
EmmaAB <66276192+EmmaAB@users.noreply.github.com>
2020-09-02 18:52:57 -0700

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,12 +1,48 @@
-Select * from wine;
+--Query for wine value: The best rated wine for less than $40
+SELECT id, variety, points, price, country, province, winery 
+FROM wine
+WHERE price <=40
+ORDER BY points DESC;
 
-SELECT wine.id, wine.country, wine.designation, wine.points, wine.price, wine.province, wine.region01 FROM wine;
+--Query for high quality (>$90) wine for less than $15
+SELECT id, variety, points, price, country, province, winery 
+FROM wine
+WHERE points >=90
+AND price<=15
+ORDER BY price;
 
+--Find countries with the cheapest wine
 SELECT country,
-COUNT(*),
-AVG(points) AS Average_points
+ROUND(AVG(price),2) AS avg_price
 FROM wine
 GROUP BY country
-ORDER BY AVG(points) DESC;
+ORDER BY avg_price;
+
+--Find countries with the highest value wine (cheapest with quality >90)
+SELECT country,
+ROUND(AVG(price),2) AS avg_price
+FROM wine
+WHERE points >=90
+GROUP BY country
+ORDER BY avg_price;
+
+--Find the lowest priced wine(s) & then query it
+SELECT ROUND(MIN(price),2) AS min_price FROM wine;
+
+SELECT id, variety, points, price, country, province, winery 
+FROM wine
+WHERE price = 4;
+
+--Find the highest priced wine(s) & then query it
+SELECT ROUND(MAX(price),2) AS min_price FROM wine;
+
+SELECT id, variety, points, price, country, province, winery 
+FROM wine
+WHERE price = 2300;
+
+--Query wines with a perfect score
+SELECT id, variety, points, price, country, province, winery 
+FROM wine
+WHERE points = 100;
 
 
